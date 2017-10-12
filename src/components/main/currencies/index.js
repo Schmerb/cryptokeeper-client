4af84c063a9410be293d0df4f8a1a89c3c153e14
Schmerb <mike.schmerbeck@gmail.com>
2017-10-12 12:00:21 -0400

    dynamically generate currency cards

--- src/components/main/currencies/index.js
+++ src/components/main/currencies/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CurrencyList from './currency-list';
