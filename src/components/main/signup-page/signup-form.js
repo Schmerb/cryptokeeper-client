2dd080c71d279e403f25d42bc42c925b156126c4
Schmerb <mike.schmerbeck@gmail.com>
2017-10-18 11:42:13 -0400

    changed env var

--- src/components/main/signup-page/signup-form.js
+++ src/components/main/signup-page/signup-form.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import {registerUser} from 'actions/users';
-import {login} from 'actions/auth';
+import { Field, reduxForm, focus } from 'redux-form';
+import { registerUser } from 'actions/users';
+import { login } from 'actions/auth';
 import Input from '../input';
-import {required, nonEmpty, matches, length, isTrimmed, email} from 'utils/validators';
+import { required, nonEmpty, matches, length, isTrimmed, email } from 'utils/validators';
 
 
 export class SignupForm extends React.Component {
