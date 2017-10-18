64aa8b7ce53999775eb4218c8448023a56d036d4
Schmerb <mike.schmerbeck@gmail.com>
2017-10-17 22:42:59 -0400

    removed backslash from config api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = 
 //     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 export const API_BASE_URL = 
-    'https://cryptocoinkeeper.herokuapp.com/' || 'http://192.168.1.112:8080/api';
+    'https://cryptocoinkeeper.herokuapp.com' || 'http://192.168.1.112:8080/api';
 // export const API_BASE_URL = 
 //     process.env.REACT_APP_API_BASE_URL || 'http://192.168.1.112:8080/api';
