e2b1099b70ec4e440402ca554e1698fe91020072
Schmerb <mike.schmerbeck@gmail.com>
2017-11-25 16:50:22 -0500

    logo redirects to dash if logged in

--- src/components/header/logo.js
+++ src/components/header/logo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Logo(props) {
     return (
         <div className={`logo ${props.path.includes('dashboard') ? 'dash' : ''}`}>
-            <Link to="/">
+            <Link to="/dashboard/portfolio">
                 CRYPTOKEEP
                 <i className="fa fa-btc" aria-hidden="true"></i>
                 R
