1d58506efa1be80aa193e909c102b6efb392505f
Schmerb <mike.schmerbeck@gmail.com>
2017-11-25 19:58:53 -0500

    set chat stream back to prod site

--- src/services/chat-stream.js
+++ src/services/chat-stream.js
@@ -6,8 +6,8 @@ import {
 
 const ioClient = require('socket.io-client')  
 // const socket   = ioClient('http://localhost:8080');
-const socket   = ioClient('http://172.20.10.2:7000');
-// const socket   = ioClient('https://cryptocoinkeeper.herokuapp.com/');
+// const socket   = ioClient('http://172.20.10.2:7000');
+const socket   = ioClient('https://cryptocoinkeeper.herokuapp.com/');
 
 let store = null;
 
