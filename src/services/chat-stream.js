7c956c0873a6ec300b0151a93dee6f8858ac8b9d
Schmerb <mike.schmerbeck@gmail.com>
2017-11-27 12:48:28 -0500

    livechat client set to heroku

--- src/services/chat-stream.js
+++ src/services/chat-stream.js
@@ -7,8 +7,8 @@ import {
 const ioClient = require('socket.io-client')  
 // const socket   = ioClient('http://localhost:8080');
 // const socket   = ioClient('http://172.20.10.2:7000');
-const socket   = ioClient('http://192.168.5.38:7000/');
-// const socket   = ioClient('https://cryptocoinkeeper.herokuapp.com/');
+// const socket   = ioClient('http://192.168.5.38:7000/');
+const socket   = ioClient('https://cryptocoinkeeper.herokuapp.com/');
 
 let store = null;
 
